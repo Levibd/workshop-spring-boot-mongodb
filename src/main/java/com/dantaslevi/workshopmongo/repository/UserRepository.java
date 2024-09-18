908b5fc1dccdd9e06db9714191a4969e89e37119
Levi Dantas <levibdantas@gmail.com>
2024-09-18 03:22:31 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/dantaslevi/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.dantaslevi.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dantaslevi.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
